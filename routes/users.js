943810e5c3b55c04521947aab84a3f9af96d15b9
Luis German Esquivel Grados <lgeldragondorado@gmail.com>
2020-09-25 07:53:28 -0500

    lowercase

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ var UsersController = require('../controllers/users');
 
 var api = express.Router();
 
-api.post('/Register', UsersController.saveUsers);
-api.post('/Login', UsersController.loginUser);
-api.put('/Update/:id', UsersController.updateUsers);
+api.post('/register', UsersController.saveUsers);
+api.post('/login', UsersController.loginUser);
+api.put('/update/:id', UsersController.updateUsers);
 
 module.exports = api;
